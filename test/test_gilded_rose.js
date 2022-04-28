f67b18afaea802bde7188812bce8e02c886a988e
Joel Hassan <joel.nhn@gmail.com>
2022-04-29 00:21:39 +0300

    Gilded Rose: should degrade quality of quality-degradable items by one

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -2,9 +2,12 @@ var { expect } = require("chai");
 var { Shop, Item } = require("../src/gilded_rose.js");
 
 describe("Gilded Rose", function () {
-  it("should foo", function () {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
+  describe("when items are not named Aged brie or Backstage passes to a TAFKAL80ETC concert", () => {
+    it("should degrade quality of item(s) by one", function () {
+      const originalQuality = 10;
+      const gildedRose = new Shop([new Item("foo", 1, originalQuality)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(originalQuality - 1);
+    });
   });
 });
