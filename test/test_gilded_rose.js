53302b4e2cf1175e67e12ab8dba747e21e869cd0
Joel Hassan <joel.nhn@gmail.com>
2022-04-30 17:32:31 +0300

    more mutation tests

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -16,7 +16,21 @@ describe("Gilded Rose", function () {
       expect(items[0].quality).to.equal(originalQuality - 1);
     });
 
-    it("should not update if quality is  0 ", () => {
+    it("should not update if quality is 0 ", () => {
+      const originalQuality = 0;
+      const gildedRose = new Shop([new Item("foo", 1, originalQuality)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(originalQuality);
+    });
+
+    it("should not update if item name is 'Sulfuras, Hand of Ragnaros'", () => {
+      const originalQuality = 1;
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, originalQuality)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(originalQuality);
+    });
+
+    it("should not update if quality is 0 ", () => {
       const originalQuality = 0;
 
       const gildedRose = new Shop([new Item("foo", 1, originalQuality)]);
@@ -40,6 +54,47 @@ describe("Gilded Rose", function () {
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(originalQuality + 3);
       });
+
+      it("should increment quality by one when quality is less than 50 and sellIn is 11", () => {
+        const sellIn = 11;
+        const originalQuality = 10;
+        const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, originalQuality)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(originalQuality + 1);
+      });
+
+      it("should increment quality by one when quality is less than 50 and sellIn is less than 11", () => {
+        const sellIn = 3;
+        const originalQuality = 48;
+        const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, originalQuality)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(originalQuality + 2);
+      });
+
+      it("should not increment quality by one when quality is 50 and sellIn is less than 11", () => {
+        const sellIn = 3;
+        const originalQuality = 49;
+        const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, originalQuality)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(50);
+      });
+
+      it("should not increment quality by one when quality is 50 and sellIn is 6", () => {
+        const sellIn = 6;
+        const originalQuality = 10;
+        const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, originalQuality)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(12);
+      });
+    });
+  });
+
+  describe("when the item quality is 50", () => {
+    it("should not increment quality", () => {
+      const originalQuality = 50;
+      const gildedRose = new Shop([new Item("Aged Brie", 1, originalQuality)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(originalQuality);
     });
   });
 
