c16666a31f0b4c16fbb8f98feba073ffa1718a29
Joel Hassan <joel.nhn@gmail.com>
2022-05-05 22:28:14 +0300

    move all logic within switch stmt

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -7,22 +7,24 @@ class Item {
 }
 
 class Shop {
-  BACKSTAGE_PASSES = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert"];
-
   constructor(items = []) {
     this.items = items;
   }
 
   updateQuality() {
-    const isABackstagePass = (item) => this.BACKSTAGE_PASSES.includes(item.name);
     const decreasesInQuality = (item) => item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros";
     const increasesInQuality = (item) => item.quality < 50;
     const isLegendaryItem = (item) => item.name == "Sulfuras, Hand of Ragnaros";
 
     return this.items.map((item) => {
+      const itemIsPastSaleDate = item.sellIn < 0;
+
       switch (item.name) {
         case "Aged Brie":
           if (increasesInQuality(item)) {
+            item.quality++;
+          }
+          if (itemIsPastSaleDate && increasesInQuality(item)) {
             item.quality++;
           }
           break;
@@ -40,36 +42,21 @@ class Shop {
               }
             }
           }
+          if (itemIsPastSaleDate) {
+            item.quality = 0;
+          }
           break;
         default:
           if (decreasesInQuality(item)) {
             item.quality--;
           }
+          if (!isLegendaryItem(item)) {
+            item.sellIn--;
+          }
+          if (itemIsPastSaleDate && decreasesInQuality(item)) {
+            item.quality--;
+          }
           break;
-      }
-
-      if (!isLegendaryItem(item)) {
-        item.sellIn--;
-      }
-
-      const itemIsPastSaleDate = item.sellIn < 0;
-
-      if (itemIsPastSaleDate) {
-        switch (item.name) {
-          case "Aged Brie":
-            if (increasesInQuality(item)) {
-              item.quality++;
-            }
-            break;
-          case "Backstage passes to a TAFKAL80ETC concert":
-            item.quality = 0;
-            break;
-          default:
-            if (decreasesInQuality(item)) {
-              item.quality--;
-            }
-            break;
-        }
       }
 
       return item;
